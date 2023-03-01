71b0aaf6f0f4abe3333adb4051fa192b136b877a
paulmunyao <paulmunyao094@gmail.com>
2023-03-01 22:09:04 +0300

    open quotation to show the importation

--- api-consumption/src/app/app.module.ts
+++ api-consumption/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } 
+import { HttpClientModule } from ''
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
