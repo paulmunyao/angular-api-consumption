aa08cfd4b751e6e92b12e90bbca80e366c7945ab
paulmunyao <paulmunyao094@gmail.com>
2023-03-03 17:40:51 +0300

    pass in body parameters

--- api-consumption/src/app/api/api.component.ts
+++ api-consumption/src/app/api/api.component.ts
@@ -28,9 +28,9 @@ export class ApiComponent implements OnInit {
   public postMethod(): void {
     let body = {
       title: 'foo',
-    body: 'bar',
-    userId: 1,
-    }
+      body: 'bar',
+      userId: 1,
+    };
     this.http
       .post('https://jsonplaceholder.typicode.com/posts', {})
       .subscribe((data) => {
