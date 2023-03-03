d4f8cb79265bdfdf30e6417a8e146c11c1f7f6af
paulmunyao <paulmunyao094@gmail.com>
2023-03-03 17:26:05 +0300

    indent code

--- api-consumption/src/app/api/api.component.ts
+++ api-consumption/src/app/api/api.component.ts
@@ -1,34 +1,36 @@
 import { HttpClient } from '@angular/common/http';
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-api',
   templateUrl: './api.component.html',
-  styleUrls: ['./api.component.css']
+  styleUrls: ['./api.component.css'],
 })
 export class ApiComponent implements OnInit {
   public getJsonValue: any;
   public postJsonValue: any;
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
   ngOnInit(): void {
     this.getMethod();
     this.postMethod();
   }
 
   // https://api.assemblyai.com/v2/transcript
-  public getMethod(): void{
-    this.http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe((data)=>{
-      console.log(data);
-      this .getJsonValue = data;
-    });
+  public getMethod(): void {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .subscribe((data) => {
+        console.log(data);
+        this.getJsonValue = data;
+      });
   }
 
-  public postMethod(): void{
-    this.http.post('https://jsonplaceholder.typicode.com/posts',{}).subscribe((data)=>{
-      console.log(data);
-      this.postJsonValue = data;
-    });
-}
+  public postMethod(): void {
+    this.http
+      .post('https://jsonplaceholder.typicode.com/posts', {})
+      .subscribe((data) => {
+        console.log(data);
+        this.postJsonValue = data;
+      });
+  }
 }
