3e3e3f275ddade2353e6f64ee19e224582e54ef0
paulmunyao <paulmunyao094@gmail.com>
2023-03-03 14:39:46 +0300

    debug code

--- api-consumption/src/app/api/api.component.ts
+++ api-consumption/src/app/api/api.component.ts
@@ -16,7 +16,7 @@ export class ApiComponent implements OnInit {
     this.getMethod();
   }
 
-  getMethod():{
+  public getMethod() void:{
     this.http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe();
   }
 }
