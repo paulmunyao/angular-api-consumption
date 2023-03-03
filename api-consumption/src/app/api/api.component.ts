ebde142f51593231107fc1ba84b9ba8e86c9aa41
paulmunyao <paulmunyao094@gmail.com>
2023-03-03 23:46:51 +0300

    add new body

--- api-consumption/src/app/api/api.component.ts
+++ api-consumption/src/app/api/api.component.ts
@@ -30,7 +30,7 @@ export class ApiComponent implements OnInit {
       title: 'The art of war',
       body: 'Sun Tzu',
       userId: 1,
-    };
+    }
     this.http
       .post('https://jsonplaceholder.typicode.com/posts', body)
       .subscribe((data) => {
