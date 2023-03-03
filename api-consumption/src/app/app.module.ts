de0ab900997983ba069b3e244edf1ef44ce6d8eb
paulmunyao <paulmunyao094@gmail.com>
2023-03-03 14:26:12 +0300

    generate test component

--- api-consumption/src/app/app.module.ts
+++ api-consumption/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiComponent } from './api/api.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApiComponent
+    ApiComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
